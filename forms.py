2675de2ebb2a189fa56d26a67b13a61ed141b58c
amyosy <yosra.amali@stud.hshl.de>
2024-07-04 00:50:31 +0200

    optimierung der htmls

--- forms.py
+++ forms.py
@@ -3,45 +3,69 @@ from wtforms import StringField, PasswordField, SubmitField, IntegerField, Selec
 from wtforms.validators import DataRequired, Email, EqualTo, Length, NumberRange
 
 
-# Registrierungsformular
+# Definition des Registrierungsformulars
 class RegistrationForm(FlaskForm):
+    # Benutzername-Feld mit DataRequired und Length Validierung
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    # E-Mail-Feld mit DataRequired und Email Validierung
     email = StringField('Email', validators=[DataRequired(), Email()])
+    # Passwort-Feld mit DataRequired Validierung
     password = PasswordField('Password', validators=[DataRequired()])
+    # Passwortbestätigungs-Feld mit DataRequired und EqualTo Validierung
     confirm_password = PasswordField('Confirm Password', validators=[DataRequired(), EqualTo('password')])
+    # Senden-Button
     submit = SubmitField('Sign Up')
 
 
-# Loginformular
+# Definition des Login-Formulars
 class LoginForm(FlaskForm):
+    # Benutzername-Feld mit DataRequired und Length Validierung
     username = StringField('Username', validators=[DataRequired(), Length(min=2, max=20)])
+    # Passwort-Feld mit DataRequired Validierung
     password = PasswordField('Password', validators=[DataRequired()])
+    # Erinnerung-Checkbox
     remember = BooleanField('Remember Me')
+    # Senden-Button
     submit = SubmitField('Login')
 
 
-# Datenformular
+# Definition des Formulars für Gesundheitsdaten
 class DataForm(FlaskForm):
+    # Blutdruck-Feld mit DataRequired Validierung
     blood_pressure = StringField('Blood Pressure', validators=[DataRequired()])
+    # Herzfrequenz-Feld mit DataRequired Validierung
     heart_rate = IntegerField('Heart Rate', validators=[DataRequired()])
+    # Gewicht-Feld mit DataRequired Validierung
     weight = FloatField('Weight', validators=[DataRequired()])
+    # Größe-Feld mit DataRequired Validierung
     height = FloatField('Height', validators=[DataRequired()])
+    # Schlaf-Feld mit DataRequired Validierung und Auswahlmöglichkeiten von 1 bis 12
     sleep = SelectField('Sleep', choices=[(str(i), str(i)) for i in range(1, 13)], validators=[DataRequired()])
+    # Stress-Feld mit DataRequired Validierung und Auswahlmöglichkeiten von 1 bis 5
     stress = SelectField('Stress', choices=[(str(i), str(i)) for i in range(1, 6)], validators=[DataRequired()])
+    # Senden-Button
     submit = SubmitField('Submit Data')
 
 
 # Formular für die Plot-Pagination
 class PlotPaginationForm(FlaskForm):
+    # Button für Gewicht-Plot
     weight_submit = SubmitField('Weight')
+    # Button für Größen-Plot
     height_submit = SubmitField('Height')
+    # Button für Herzfrequenz-Plot
     heart_rate_submit = SubmitField('Heart Rate')
+    # Button für Blutdruck-Plot
     blood_pressure_submit = SubmitField('Blood Pressure')
+    # Button für Schlaf-Plot
     sleep_submit = SubmitField('Sleep')
+    # Button für Stress-Plot
     stress_submit = SubmitField('Stress')
 
 
-# Erinnerungsformular
+# Definition des Formulars für Erinnerungen
 class ReminderForm(FlaskForm):
+    # Erinnerungs-Feld mit DataRequired Validierung
     reminder = StringField('Reminder', validators=[DataRequired()])
+    # Senden-Button
     submit = SubmitField('Add Reminder')
--- init.py
+++ init.py
@@ -6,24 +6,32 @@ import os
 
 # Initialisieren der SQLAlchemy-Datenbankinstanz
 db = SQLAlchemy()
-bcrypt = Bcrypt()  # Initialisieren der Bcrypt-Instanz für Passwort-Hashing
+# Initialisieren der Bcrypt-Instanz für Passwort-Hashing
+bcrypt = Bcrypt()
 
 
 # Funktion zur Erstellung der Flask-Anwendung
 def create_app():
+    # Erstellen der Flask-Anwendung
     app = Flask(__name__)
+    # Konfiguration des geheimen Schlüssels für die Sitzungsverwaltung
     app.config['SECRET_KEY'] = 'supersecretkey'
+    # Konfiguration der Datenbank-URI; verwendet die Umgebungsvariable 'DATABASE_URL' oder eine SQLite-Datenbank als Fallback
     app.config['SQLALCHEMY_DATABASE_URI'] = os.getenv('DATABASE_URL', 'sqlite:///site.db')
 
     # Initialisieren der Datenbankinstanz mit der Flask-Anwendung
     db.init_app(app)
+    # Initialisieren der Bcrypt-Instanz mit der Flask-Anwendung
     bcrypt.init_app(app)
+    # Initialisieren der Migrate-Instanz mit der Flask-Anwendung und der Datenbankinstanz
     migrate = Migrate(app, db)
 
-    # Anwendungs-Kontext für die Initialisierung und Importieren der Formulare und Modelle
+    # Anwendungs-Kontext für die Initialisierung des Schemas und Importieren der Formulare und Modelle
     with app.app_context():
         from forms import RegistrationForm, LoginForm, DataForm, ReminderForm
         from models import User, HealthData, Reminder
-        db.create_all()  # Erstellen aller Datenbanktabellen basierend auf den Modellen
+        # Erstellen aller Datenbanktabellen basierend auf den Modellen
+        db.create_all()
 
+    # Rückgabe der erstellten Flask-Anwendung
     return app
